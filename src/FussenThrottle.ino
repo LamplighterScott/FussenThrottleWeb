37179fb55abf889992602798bfc3eb407b815790
Scott Eaton <scotteaton@Scotts-MacBook-Pro.local>
2020-04-11 19:53:27 -0700

    Added version, cleaned-up OTA comments

--- src/FussenThrottle.ino
+++ src/FussenThrottle.ino
@@ -1,7 +1,8 @@
 /*
  * Arduino ESP8266 DCC++ over WiFi Throttle
- *
  * Fussen RR
+ * Version 1.0.0  April 11, 2020
+ * By Scott Eaton
  */
 
 #include <ESP8266WiFi.h> // includes WiFiClient.h
@@ -88,50 +89,6 @@ void loop(void){
   server.handleClient();          //Handle client requests
 }
 
-
-//==============================================================
-//                     Setup Functions
-//==============================================================
-
-/*void startOTA(){
-  Serial.println("Connecting ...");
-  Serial.println("\nBooting");
-  while (WiFi.waitforConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  // Port defaults to ESP8266
-  // ArduinoOTA.setPort(8266);
-
-  // HostName defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("FussenRR);
-
-  // No authentication by defaults
-  // ArdinoOTA.setPassword(password);  // Current disabled in Config.h
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("OTA ready");
-
-}*/
 
 void startWiFi() {
   WiFi.mode(WIFI_AP);
